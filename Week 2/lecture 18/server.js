9c88c7474e250ee1680db1c2706df1624f58aebd
Mohd Hammaz <mohd.ashad200881@gmail.com>
2026-09-23 21:57:57 +0530

    Refactor error handling and response messages

--- Week 2/lecture 18/server.js
+++ Week 2/lecture 18/server.js
@@ -1,33 +1,42 @@
-
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    try{
-        throw new Error("something went wrong");
-    }catch(error){
-        res.status(500).json({success:false, message:"something went wrong"});
+app.get("/", (req, res) => {
+    try {
+        throw new Error("Something went wrong");
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong"
+        });
     }
-})
+});
 
-
-
-app.get("/:id",(req , res)=>{
+app.get("/:id", (req, res) => {
     const id = req.params.id;
-    try{
-        if (id ==="1234"){
-            res.send("valid id ")
-        }else{
-            throw new Error("Invalid id")
+
+    try {
+        if (id === "1234") {
+            res.send("Valid ID");
+        } else {
+            throw new Error("Invalid ID");
         }
-    }catch(error){
-        res.status(400).json({ success: false, message: "Invalid ID" });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Invalid ID"
+        });
     }
-})
+});
 
-app.use((req,res)=>{
-    res.status(404).json({success:false, message:"Pafe not found"})
-})
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Page not found"
+    });
+});
 
-
-app.listen(PORT,()=> console.log('server is running'));
+app.listen(PORT, () => {
+    console.log("Server is running");
+});
